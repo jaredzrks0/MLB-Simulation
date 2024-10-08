ef5781abf2da1bf01eb25e656f5cc7a3c6a6740c
Jared Zirkes <jaredzrks0@gmail.com>
2024-10-08 18:18:24 -0400

    Completed lineup scraping for both Rotowire and MLB

--- get_lineups.py
+++ get_lineups.py
@@ -1,7 +1,57 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def scrape_lineups(url='https://www.rotowire.com/baseball/daily-lineups.php'):
+def mlb_scrape(url='https://www.mlb.com/starting-lineups'):
+    response = requests.get(url)
+    soup = bs(response.content, features="lxml")
+
+    lineups = {}
+    days_games = []
+
+    all_games = soup.find_all('div', {'class':'starting-lineups__matchup'})
+
+    i = 0
+    for game in all_games:
+        lineups[i] = {}
+
+        location = game.find_all('div', {'class':'starting-lineups__game-location'})[0].text.strip()
+
+        home_team = game.find_all('span', {'class': 'starting-lineups__team-name starting-lineups__team-name--home'})[0].text.split()[0]
+        lineups[i]['home_team'] = home_team
+        away_team = game.find_all('span', {'class': 'starting-lineups__team-name starting-lineups__team-name--away'})[0].text.split()[0]
+        lineups[i]['away_team'] = away_team
+
+        home_lineup = game.find_all('ol', {'class':'starting-lineups__team starting-lineups__team--home'})[0]
+        away_lineup = game.find_all('ol', {'class':'starting-lineups__team starting-lineups__team--away'})[0]
+
+        home_pitcher = game.find_all('a', {'class':'starting-lineups__pitcher--link'})[3].text
+        away_pitcher = game.find_all('a', {'class':'starting-lineups__pitcher--link'})[1].text
+
+        home_players = home_lineup.find_all('li', {'class':'starting-lineups__player'})
+        home_player_names = [player.a.text for player in home_players]
+        home_player_positions = [player.span.text.split(") ")[-1].strip() for player in home_players]
+        home_player_ids = [player.a['href'].split("-")[-1] for player in home_players]
+
+        away_players = away_lineup.find_all('li', {'class':'starting-lineups__player'})
+        away_player_names = [player.a.text for player in away_players]
+        away_player_positions = [player.span.text.split(") ")[-1].strip() for player in away_players]
+        away_player_ids = [player.a['href'].split("-")[-1] for player in away_players]
+
+        # Insert into Dictionary and List
+        lineups[i]['home_pitcher'] = home_pitcher
+        lineups[i]['away_pitcher'] = away_pitcher
+        lineups[i]['stadium'] = location
+
+        lineups[i]['home_lineup'] = {n+1:{'position':home_player_positions[n], 'player':home_player_names[n], 'id':home_player_ids[n]} for n in range(len(home_player_positions))}
+        lineups[i]['away_lineup'] = {n+1:{'position':away_player_positions[n], 'player':away_player_names[n], 'id':away_player_ids[n]} for n in range(len(away_player_positions))}
+
+        days_games.append((home_team, away_team))
+        i += 1
+
+    return {'games':days_games, 'lineups':lineups}
+
+
+def rotowire_scrape(url='https://www.rotowire.com/baseball/daily-lineups.php'):
     response = requests.get(url)
     soup = bs(response.content, 'html')
 
@@ -57,5 +107,3 @@ def scrape_lineups(url='https://www.rotowire.com/baseball/daily-lineups.php'):
         i += 1
 
     return lineups
-
-
