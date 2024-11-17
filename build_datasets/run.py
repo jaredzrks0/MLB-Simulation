1bc2080956f1632648b2dc6b4b0c2aada3252cd1
Jared Zirkes <jaredzrks0@gmail.com>
2024-11-16 20:13:21 -0500

    Set up script to build datasets with different windows

--- build_datasets/run.py
+++ build_datasets/run.py
@@ -20,24 +20,37 @@ warnings.simplefilter("ignore")
 
 if __name__ == '__main__':
     
+    # Pull in the raw pitches for dataset building
     file_str = "Data/raw_pitches_2016-2018"
 
     with open(file_str, 'rb') as file:
         raw_pitches = pkl.load(file)
 
-    save = True
+    # Define the different rolling window settings
+    rolling_windows = [[504],
+                       [75],
+                       [75, 504],
+                       [40, 504],
+                       [10, 504],
+                       [40, 75],
+                       [10, 75],
+                       [40, 75, 504],
+                       [10, 40, 504],
+                       [10, 75, 504],
+                       [10, 40, 74, 504]]
+    
+    for window in rolling_windows:
+        # Define the settings for the dataset builder run
+        dataset = dataset_builder(rolling_windows=window, verbose=False)
+        year_suffix = '2016-2018'
+        rolling_window_suffix = '_'.join([str(pa) for pa in dataset.rolling_windows])
+        dataset_suffix = f'{year_suffix}_rolling_windows_{rolling_window_suffix}'
 
-    dataset = dataset_builder()
-
-    year_suffix = '2016-2018'
-    rolling_window_suffix = '_'.join([str(pa) for pa in dataset.rolling_windows])
-
-    dataset_suffix = f'{year_suffix}_rolling_windows_{rolling_window_suffix}'
-
-    df = dataset.build_training_dataset(raw_pitches, suffix=dataset_suffix,
-                                   save_cleaned=save, save_coefficients=save,
-                                   save_dataset=save, save_training_dataset=save,
-                                   local_save=True, online_save=False)
+        # Create the dataset
+        df = dataset.build_training_dataset(raw_pitches, suffix=dataset_suffix,
+                                    save_cleaned=False, save_coefficients=False,
+                                    save_dataset=False, save_training_dataset=True,
+                                    local_save=True, online_save=False)
 
 
     
